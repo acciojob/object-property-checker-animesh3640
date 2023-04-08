30d5878cd1ce7e1f5ec9197868ffe73a7a726766
animesh3640 <127398605+animesh3640@users.noreply.github.com>
2023-04-08 16:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 	for(let i in sampleObject){
-		if (i==undefined) {
-			return false;
-		}
-		else{
+		if (i==key) {
 			return true;
 		}
+		else{
+			return false;
+		}
+		return i;
 	}
 }
 
-Do not change the code below
+//Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+
+
